f65ea4bc30422de49d6346045cbf46665bcfea8a
Francisco Bandeira <francisco.frazzon@outlook.com>
2026-02-27 16:48:17 -0300

    alterando a massa de dados

--- cypress/e2e/contato.cy.js
+++ cypress/e2e/contato.cy.js
@@ -1,52 +1,70 @@
 describe('Funcionalidade: Contato', () => {
 
+const campos = {
+  NOME: '#name',
+  EMAIL: '#email',
+  ASSUNTO: '#subject',
+  MENSAGEM: '#message',
+  BOTAO: '#btn-submit',
+  ALERTA: '#alert-container'
+};
+
+const pessoa = {
+  nome: 'João Silva',
+  email: 'joao.silva@example.com',
+  selected: 'Sugestões',
+  mensagem: 'Gostaria de sugerir a inclusão de mais opções de pagamento.',
+  sucesso: 'Contato enviado com sucesso!',
+  erroNome: 'Por favor, preencha o campo Nome.',
+  erroEmail: 'Por favor, preencha o campo E-mail.',
+  erroAssunto: 'Por favor, selecione o Assunto.',
+  erroMensagem: 'Por favor, escreva sua Mensagem.'
+};
+
   beforeEach(() => {
     cy.visit('index.html');
   });
   it('Deve preencher o formulário de contato com sucesso', () => {
-    cy.get('#name').type('João Silva');
-    cy.get('#email').type('joao.silva@example.com');
-    cy.get('#subject').select('Sugestões');
-    cy.get('#message').type('Gostaria de sugerir a inclusão de mais opções de pagamento.');
-    cy.get('#btn-submit').click();
-    cy.get('#alert-container').should('be.visible').and('contain', 'Contato enviado com sucesso!');
+    cy.get(campos.NOME).type(pessoa.nome);
+    cy.get(campos.EMAIL).type(pessoa.email);
+    cy.get(campos.ASSUNTO).select(pessoa.selected);
+    cy.get(campos.MENSAGEM).type(pessoa.mensagem);
+    cy.get(campos.BOTAO).click();
+    cy.get(campos.ALERTA).should('be.visible').and('contain', pessoa.sucesso);
   });
 
   it('Deve validar mensagem de erro ao enviar sem preencher nome', () => {
-    cy.get('#email').type('joao.silva@example.com');
-    cy.get('#subject').select('Sugestões');
-    cy.get('#message').type('Gostaria de sugerir a inclusão de mais opções de pagamento.');
-    cy.get('#btn-submit').click();
-    cy.get('#alert-container').should('be.visible').and('contain', 'Por favor, preencha o campo Nome.');
+    cy.get(campos.EMAIL).type('joao.silva@example.com');
+    cy.get(campos.ASSUNTO).select(pessoa.selected);
+    cy.get(campos.MENSAGEM).type(pessoa.mensagem);
+    cy.get(campos.BOTAO).click();
+    cy.get(campos.ALERTA).should('be.visible').and('contain', pessoa.erroNome );
   });
 
   it('Deve validar mensagem de erro ao enviar sem preencher email', () => {
-    cy.get('#name').type('João Silva');
-    cy.get('#email').clear();
-    cy.get('#subject').select('Sugestões');
-    cy.get('#message').type('Gostaria de sugerir a inclusão de mais opções de pagamento.');
-    cy.get('#btn-submit').click();
-    cy.get('#alert-container').should('be.visible').and('contain', 'Por favor, preencha o campo E-mail.');
+    cy.get(campos.NOME).type('João Silva');
+    cy.get(campos.EMAIL).clear();
+    cy.get(campos.ASSUNTO).select('Sugestões');
+    cy.get(campos.MENSAGEM).type(pessoa.mensagem);
+    cy.get(campos.BOTAO).click();
+    cy.get(campos.ALERTA).should('be.visible').and('contain', pessoa.erroEmail);
   });
 
   it('Deve validar mensagem de erro ao enviar sem preencher assunto', () => {
-    cy.get('#name').type('João Silva');cy.get('#email').type('joao.silva@example.com');
-    cy.get('#subject').select('Selecione o assunto...');
-    cy.get('#message').type('Gostaria de sugerir a inclusão de mais opções de pagamento.');
-    cy.get('#btn-submit').click();
-    cy.get('#alert-container').should('be.visible').and('contain', 'Por favor, selecione o Assunto.');
+    cy.get(campos.NOME).type('João Silva'); cy.get(campos.EMAIL).type('joao.silva@example.com');
+    cy.get(campos.ASSUNTO).select('Selecione o assunto...');
+    cy.get(campos.MENSAGEM).type(pessoa.mensagem);
+    cy.get(campos.BOTAO).click();
+    cy.get(campos.ALERTA).should('be.visible').and('contain', pessoa.erroAssunto);
   });
 
   it('Deve validar mensagem de erro ao enviar sem preencher a mensagem', () => {
-    cy.get('#name').type('João Silva');
-    cy.get('#email').type('joao.silva@example.com');
-    cy.get('#subject').select('Sugestões');
-    cy.get('#message').clear();
-    cy.get('#btn-submit').click();
-    cy.get('#alert-container').should('be.visible').and('contain', 'Por favor, escreva sua Mensagem.');
+    cy.get(campos.NOME).type('João Silva');
+    cy.get(campos.EMAIL).type('joao.silva@example.com');
+    cy.get(campos.ASSUNTO).select('Sugestões');
+    cy.get(campos.MENSAGEM).clear();
+    cy.get(campos.BOTAO).click();
+    cy.get(campos.ALERTA).should('be.visible').and('contain', pessoa.erroMensagem);
   });
-
-
-
 
 });
