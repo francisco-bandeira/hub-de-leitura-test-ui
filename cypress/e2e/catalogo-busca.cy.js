cd1988a598ff3e42034efe525a7b773835b96502
Francisco Bandeira <francisco.frazzon@outlook.com>
2026-03-02 18:30:13 -0300

    busca de livros usando cy.fixture

--- cypress/e2e/catalogo-busca.cy.js
+++ cypress/e2e/catalogo-busca.cy.js
@@ -5,14 +5,31 @@ describe('Funcionalidade: Busca no Catálogo', () => {
   beforeEach(() => {
     cy.visit('catalog.html')
   });
-it('Deve buscar o livro 1984 com sucesso', () => {
-   cy.get('#search-input').type('1984')
-  cy.get('.card-title').should('contain', '1984').and('be.visible')
-});
+  it('Deve buscar o livro 1984 com sucesso', () => {
+    cy.get('#search-input').type('1984')
+    cy.get('.card-title').should('contain', '1984').and('be.visible')
+  });
 
-it('Deve buscar o livro do arquivo de massa de dados', () => {
-   cy.get('#search-input').type(catalogo.livros[1].titulo)
-  cy.get('.card-title').should('contain', catalogo.livros[1].titulo).and('be.visible')
-});
+  it('Deve buscar o livro do arquivo de massa de dados', () => {
+    cy.get('#search-input').type(catalogo[1].titulo)
+    cy.get('.card-title').should('contain', catalogo[1].titulo).and('be.visible')
+  });
+
+  it('deve fazer uma busca de livros usando fixtures', () => {
+    cy.fixture("livros").then((catalogo) => {
+      const livro = catalogo[2]
+      cy.get('#search-input').type(livro.titulo)
+      cy.get('.card-title').should('contain', livro.titulo).and('be.visible')
+    })
+  });
+
+  it.only('deve validar toda a lista de livros na fixture', () => {
+    cy.fixture("livros").then((catalogo) => {
+      catalogo.forEach(livro => {
+        cy.get('#search-input').clear().type(livro.titulo)
+        cy.get('.card-title').should('contain', livro.titulo).and('be.visible')
+      })
+  })
+  });
 
 });
